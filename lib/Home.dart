015128b30f8361f894f5447308da0e1c382a18c5
Sunit Roy <sunitroy39@gmail.com>
2020-07-03 10:34:19 +0530

    Initial commit

---
+++ lib/Home.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ToDo App'),
+      ),
+      body: getListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('Fab Clicked');
+        },
+        child: Icon(Icons.add ,color: Colors.white,),
+        tooltip:'Add Note',
+      ),
+    );
+  }
+}
+
+ListView getListView() {
+  var count;
+  return ListView.builder(
+    itemCount: count,
+    itemBuilder: (BuildContext context, int index) {
+      return Card(
+        color: Colors.white,
+        elevation: 2.0,
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.amber,
+            child: Icon(Icons.keyboard_arrow_right, color:  Colors.white,),
+          ),
+          title: Text(
+            'Dummy Title',
+            style: TextStyle(),
+          ),
+          subtitle: Text(
+            'Dummy Title',
+            style: TextStyle(),
+          ),
+          trailing: Icon(
+            Icons.delete,
+            color: Colors.grey,
+          ),
+          onTap: () {
+            debugPrint('List item selected');
+          },
+        ),
+      );
+    },
+  );
+}
