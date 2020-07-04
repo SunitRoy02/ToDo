cdcaad8d5136912513313f0c7d02250e95224dd5
Sunit Roy <sunitroy39@gmail.com>
2020-07-04 18:25:28 +0530

    updated

--- lib/db_helper/database_helper.dart
+++ lib/db_helper/database_helper.dart
@@ -3,17 +3,11 @@ import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:todo/models/notes.dart';
+import 'package:todo/models/note.dart';
 
 class DatabaseHelper {
-  //Singleton Database Helper
-  static DatabaseHelper _databaseHalper;
-
-  //Singleton Database
-  static Database _database;
-
-  //Named Constructor to create instance of database
-  DatabaseHelper._createInstance();
+  static DatabaseHelper _databaseHelper; // Singleton DatabaseHelper
+  static Database _database; // Singleton Database
 
   String noteTable = 'note_table';
   String colId = 'id';
@@ -22,73 +16,94 @@ class DatabaseHelper {
   String colPriority = 'priority';
   String colDate = 'date';
 
+  DatabaseHelper._createInstance(); // Named constructor to create instance of DatabaseHelper
+
   factory DatabaseHelper() {
-    if (_databaseHalper == null) {
-      _databaseHalper = DatabaseHelper._createInstance();
+    if (_databaseHelper == null) {
+      _databaseHelper = DatabaseHelper
+          ._createInstance(); // This is executed only once, singleton object
     }
-    return _databaseHalper;
+    return _databaseHelper;
   }
 
   Future<Database> get database async {
-    if (_database != null) {
-      _database = await intializeDatabase();
+    if (_database == null) {
+      _database = await initializeDatabase();
     }
     return _database;
   }
 
-  get db => null;
-
-  Future<Database> intializeDatabase() async {
-    // get the directory path
+  Future<Database> initializeDatabase() async {
+    // Get the directory path for both Android and iOS to store database.
     Directory directory = await getApplicationDocumentsDirectory();
     String path = directory.path + 'notes.db';
-    // open/create the database at a given path
+
+    // Open/create the database at a given path
     var notesDatabase =
         await openDatabase(path, version: 1, onCreate: _createDb);
     return notesDatabase;
   }
 
-  // creating Database
-  void _createDb(Database db, int Version) async {
+  void _createDb(Database db, int newVersion) async {
     await db.execute(
-        'CREATE TABLE $noteTable($colId INTEGER PRIMARY KEY AUTOINCREMENT, $colTitle TEXT,' +
-            '$colDescription TEXT,$colPriority INTEGER,$colDate TEXT');
+        'CREATE TABLE $noteTable($colId INTEGER PRIMARY KEY AUTOINCREMENT, $colTitle TEXT, '
+        '$colDescription TEXT, $colPriority INTEGER, $colDate TEXT)');
   }
 
-  //Fatch Operation
+  // Fetch Operation: Get all note objects from database
   Future<List<Map<String, dynamic>>> getNoteMapList() async {
+    Database db = await this.database;
+
+//		var result = await db.rawQuery('SELECT * FROM $noteTable order by $colPriority ASC');
     var result = await db.query(noteTable, orderBy: '$colPriority ASC');
     return result;
   }
 
-  //Insert Operation
+  // Insert Operation: Insert a Note object to database
   Future<int> insertNote(Note note) async {
     Database db = await this.database;
     var result = await db.insert(noteTable, note.toMap());
     return result;
   }
 
-  //Update Operation
+  // Update Operation: Update a Note object and save it to database
   Future<int> updateNote(Note note) async {
     var db = await this.database;
-    var result = await db.update(noteTable, note.toMap(),
-        where: '$colId= ?', whereArgs: [note.id]);
+    var result = await db.update(
+        noteTable, note.toMap(), where: '$colId = ?', whereArgs: [note.id]);
     return result;
   }
 
-  //Delete Operation
-  Future<int> deletetNote(int id) async {
+  // Delete Operation: Delete a Note object from database
+  Future<int> deleteNote(int id) async {
     var db = await this.database;
-    var result = await db.rawDelete('DELETE FROM $noteTable WERE $colId = $id');
+    int result = await db.rawDelete(
+        'DELETE FROM $noteTable WHERE $colId = $id');
     return result;
   }
 
-  //get number of note obj in database
+  // Get number of Note objects in database
   Future<int> getCount() async {
-    var db = await this.database;
-    List<Map<String, dynamic>> x =
-        await db.rawQuery('SELECT COUNT (*) from $noteTable');
+    Database db = await this.database;
+    List<Map<String, dynamic>> x = await db.rawQuery(
+        'SELECT COUNT (*) from $noteTable');
     int result = Sqflite.firstIntValue(x);
     return result;
   }
-} // end od class
+
+  // Get the 'Map List' [ List<Map> ] and convert it to 'Note List' [ List<Note> ]
+  Future<List<Note>> getNoteList() async {
+    var noteMapList = await getNoteMapList(); // Get 'Map List' from database
+    int count = noteMapList
+        .length; // Count the number of map entries in db table
+
+    List<Note> noteList = List<Note>();
+    // For loop to create a 'Note List' from a 'Map List'
+    for (int i = 0; i < count; i++) {
+      noteList.add(Note.fromMapObject(noteMapList[i]));
+    }
+
+    return noteList;
+  }
+
+}
--- lib/note_detail.dart
+++ lib/note_detail.dart
@@ -1,142 +1,278 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:todo/db_helper/database_helper.dart';
+import 'package:todo/models/note.dart';
 
-class NoteDetails extends StatefulWidget {
-  String appBarTitle = 'Task';
-  BuildContext context;
+class NoteDetail extends StatefulWidget {
+  final String appBarTitle;
+  final Note note;
 
-  NoteDetails(context, this.appBarTitle);
+  NoteDetail(this.note, this.appBarTitle);
 
   @override
-  NoteDetailsState createState() => NoteDetailsState(context, this.appBarTitle);
+  State<StatefulWidget> createState() {
+    return NoteDetailState(this.note, this.appBarTitle);
+  }
 }
 
-class NoteDetailsState extends State<NoteDetails> {
-  static var _priority = ['High', 'Low'];
-  TextEditingController task = TextEditingController();
-  TextEditingController shadule = TextEditingController();
+class NoteDetailState extends State<NoteDetail> {
+
+  static var _priorities = ['High', 'Low'];
+
+  DatabaseHelper helper = DatabaseHelper();
 
   String appBarTitle;
+  Note note;
 
-  NoteDetailsState(context, this.appBarTitle);
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  NoteDetailState(this.note, this.appBarTitle);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(appBarTitle),
-      ),
-      body: Padding(
-        padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
-        child: ListView(
-          children: <Widget>[
-            // First element dropdown menu
-            ListTile(
-              title: DropdownButton(
-                  items: _priority.map((String dropDownStringItem) {
-                    return DropdownMenuItem<String>(
-                      value: dropDownStringItem,
-                      child: Text(dropDownStringItem),
-                    );
-                  }).toList(),
-//                style: textStyle,
-                  value: 'Low',
-                  onChanged: (valueSelectedByUser) {
-                    setState(() {
-                      debugPrint('User selected $valueSelectedByUser');
-                    });
-                  }),
-            ),
+    TextStyle textStyle = Theme
+        .of(context)
+        .textTheme
+        .title;
 
-            //second item
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 15.0),
-              child: TextField(
-                controller: task,
-                style: TextStyle(color: Colors.black87),
-                onChanged: (value) {
-                  debugPrint('Adding task');
-                },
-                decoration: InputDecoration(
-                  labelText: 'Task',
-                  labelStyle: TextStyle(color: Colors.grey, fontSize: 20.0),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0)),
+    titleController.text = note.title;
+    descriptionController.text = note.description;
+
+    return WillPopScope(
+
+        onWillPop: () {
+          // Write some code to control things, when user press Back navigation button in device navigationBar
+          moveToLastScreen();
+        },
+
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(appBarTitle),
+            leading: IconButton(icon: Icon(
+                Icons.arrow_back),
+                onPressed: () {
+                  // Write some code to control things, when user press back button in AppBar
+                  moveToLastScreen();
+                }
+            ),
+          ),
+
+          body: Padding(
+            padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
+            child: ListView(
+              children: <Widget>[
+
+                // First element
+                ListTile(
+                  title: DropdownButton(
+                      items: _priorities.map((String dropDownStringItem) {
+                        return DropdownMenuItem<String>(
+                          value: dropDownStringItem,
+                          child: Text(dropDownStringItem),
+                        );
+                      }).toList(),
+
+                      style: textStyle,
+
+                      value: getPriorityAsString(note.priority),
+
+                      onChanged: (valueSelectedByUser) {
+                        setState(() {
+                          debugPrint('User selected $valueSelectedByUser');
+                          updatePriorityAsInt(valueSelectedByUser);
+                        });
+                      }
+                  ),
                 ),
-              ),
-            ),
 
-            // third element
-
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 15.0),
-              child: TextField(
-                controller: shadule,
-                style: TextStyle(color: Colors.black87),
-                onChanged: (value) {
-                  debugPrint('time task');
-                },
-                decoration: InputDecoration(
-                  labelText: 'Date & Time',
-                  labelStyle: TextStyle(color: Colors.grey),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0)),
+                // Second Element
+                Padding(
+                  padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  child: TextField(
+                    controller: titleController,
+                    style: textStyle,
+                    onChanged: (value) {
+                      debugPrint('Something changed in Title Text Field');
+                      updateTitle();
+                    },
+                    decoration: InputDecoration(
+                        labelText: 'Title',
+                        labelStyle: textStyle,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0)
+                        )
+                    ),
+                  ),
                 ),
-              ),
-            ),
 
-            //forth element
-            // Fourth Element
-            Padding(
-              padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-              child: Container(
-                height: 70,
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Container(
+                // Third Element
+                Padding(
+                  padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  child: TextField(
+                    controller: descriptionController,
+                    style: textStyle,
+                    onChanged: (value) {
+                      debugPrint('Something changed in Description Text Field');
+                      updateDescription();
+                    },
+                    decoration: InputDecoration(
+                        labelText: 'Description',
+                        labelStyle: textStyle,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0)
+                        )
+                    ),
+                  ),
+                ),
+
+                // Fourth Element
+                Padding(
+                  padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
                         child: RaisedButton(
-                          padding: EdgeInsets.all(15.0),
-                          color: Theme.of(context).primaryColorDark,
-                          textColor: Theme.of(context).primaryColorLight,
+                          color: Theme
+                              .of(context)
+                              .primaryColorDark,
+                          textColor: Theme
+                              .of(context)
+                              .primaryColorLight,
                           child: Text(
                             'Save',
-                            style: TextStyle(fontSize: 25.0),
+                            textScaleFactor: 1.5,
                           ),
                           onPressed: () {
                             setState(() {
                               debugPrint("Save button clicked");
+                              _save();
                             });
                           },
                         ),
                       ),
-                    ),
-                    Container(
-                      width: 5.0,
-                    ),
-                    Expanded(
-                      child: RaisedButton(
-                        padding: const EdgeInsets.all(15.0),
-                        color: Theme.of(context).primaryColorDark,
-                        textColor: Theme.of(context).primaryColorLight,
-                        child: Text(
-                          'Delete',
-                          style: TextStyle(fontSize: 25.0),
+
+                      Container(width: 5.0,),
+
+                      Expanded(
+                        child: RaisedButton(
+                          color: Theme
+                              .of(context)
+                              .primaryColorDark,
+                          textColor: Theme
+                              .of(context)
+                              .primaryColorLight,
+                          child: Text(
+                            'Delete',
+                            textScaleFactor: 1.5,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              debugPrint("Delete button clicked");
+                              _delete();
+                            });
+                          },
                         ),
-                        onPressed: () {
-                          setState(() {
-                            debugPrint("Delete button clicked");
-                          });
-                        },
                       ),
-                    ),
-                  ],
+
+                    ],
+                  ),
                 ),
-              ),
+
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+
+        ));
+  }
+
+  void moveToLastScreen() {
+    Navigator.pop(context, true);
+  }
+
+  // Convert the String priority in the form of integer before saving it to Database
+  void updatePriorityAsInt(String value) {
+    switch (value) {
+      case 'High':
+        note.priority = 1;
+        break;
+      case 'Low':
+        note.priority = 2;
+        break;
+    }
+  }
+
+  // Convert int priority to String priority and display it to user in DropDown
+  String getPriorityAsString(int value) {
+    String priority;
+    switch (value) {
+      case 1:
+        priority = _priorities[0]; // 'High'
+        break;
+      case 2:
+        priority = _priorities[1]; // 'Low'
+        break;
+    }
+    return priority;
+  }
+
+  // Update the title of Note object
+  void updateTitle() {
+    note.title = titleController.text;
+  }
+
+  // Update the description of Note object
+  void updateDescription() {
+    note.description = descriptionController.text;
+  }
+
+  // Save data to database
+  void _save() async {
+    moveToLastScreen();
+
+    note.date = DateFormat.yMMMd().format(DateTime.now());
+    int result;
+    if (note.id != null) { // Case 1: Update operation
+      result = await helper.updateNote(note);
+    } else { // Case 2: Insert Operation
+      result = await helper.insertNote(note);
+    }
+
+    if (result != 0) { // Success
+      _showAlertDialog('Status', 'Note Saved Successfully');
+    } else { // Failure
+      _showAlertDialog('Status', 'Problem Saving Note');
+    }
+  }
+
+  void _delete() async {
+    moveToLastScreen();
+
+    // Case 1: If user is trying to delete the NEW NOTE i.e. he has come to
+    // the detail page by pressing the FAB of NoteList page.
+    if (note.id == null) {
+      _showAlertDialog('Status', 'No Note was deleted');
+      return;
+    }
+
+    // Case 2: User is trying to delete the old note that already has a valid ID.
+    int result = await helper.deleteNote(note.id);
+    if (result != 0) {
+      _showAlertDialog('Status', 'Note Deleted Successfully');
+    } else {
+      _showAlertDialog('Status', 'Error Occured while Deleting Note');
+    }
+  }
+
+  void _showAlertDialog(String title, String message) {
+    AlertDialog alertDialog = AlertDialog(
+      title: Text(title),
+      content: Text(message),
+    );
+    showDialog(
+        context: context,
+        builder: (_) => alertDialog
     );
   }
+
 }
